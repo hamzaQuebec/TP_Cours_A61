d0db3f7434b6ee28e966440793c69e783abab8c9
hamzaQuebec <hamza.badr@gmail.com>
2024-02-04 20:38:03 -0500

    12- Ajout de schéma de validation à l'API Flask: Ajout du fichier validation dans /ml_api/api/ Ajout du fichier test_data_predictions.csv dans /ml_api Ajout du fichier test_validation dans /ml_api/tests Modification des fichiers suivants: api/config.py , api/controller.py, requiermennts.txt (ajour du package marshmallow) et tests/test_controller.py

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-from io import StringIO
 
 from regression_model.processing.data_management import load_pipeline
 from regression_model.config import config
@@ -19,7 +18,7 @@ _price_pipe = load_pipeline(file_name=pipeline_file_name)
 def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
-    data = pd.read_json(StringIO(input_data))
+    data = pd.DataFrame(input_data)
     validated_data = validate_inputs(input_data=data)
     prediction = _price_pipe.predict(validated_data[config.FEATURES])
     output = np.exp(prediction)
